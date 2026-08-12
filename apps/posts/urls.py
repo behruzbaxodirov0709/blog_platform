d2b2cc67d48773e5b7b4db069953bdd6015d9be6
Behruz Baxodirov <bekzodovich@gmail.com>
2026-08-12 14:44:34 +0500

    "Comment section tugatildi, o'chirish va tahrirlash funksiyalari qo'shildi"

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path(route="", view=views.PostListView.as_view(), name="post_list"),
-    path(route="<int:pk>", view=views.PostDetailView.as_view(), name="post_detail"),
+    path(route="<int:pk>/", view=views.PostDetailView.as_view(), name="post_detail"),
     path(route="create/", view=views.PostCreateView.as_view(), name="post_create"),
     path(route="<int:pk>/update/", view=views.PostUpdateView.as_view(), name="post_update"),
-    path(route="<int:pk>/delete/", view=views.PostDeleteView.as_view(), name="post_delete")
+    path(route="<int:pk>/delete/", view=views.PostDeleteView.as_view(), name="post_delete"),
+    path(route="comment/<int:pk>/update/", view=views.CommentUpdateView.as_view(), name="comment_update"),
+    path(route="comment/<int:pk>/delete/", view=views.CommentDeleteView.as_view(), name="comment_delete")
 ]
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -75,38 +75,111 @@ class PostCreateView(View):
 class PostUpdateView(View):
     def get(self, request, pk):
         post = Post.objects.get(pk=pk)
-        form = forms.PostCreateForm(instance=post)
-        return render(
-            request=request,
-            template_name="posts/post_update.html",
-            context = {"form":form}
-        )
+        if post.author == request.user:    
+            form = forms.PostCreateForm(instance=post)
+            return render(
+                request=request,
+                template_name="posts/post_update.html",
+                context = {"form":form}
+            )
+
+        else:
+            return redirect("main_page")
+
+
 
 
     def post(self, request, pk):
         post = Post.objects.get(pk=pk)
-        form = forms.PostCreateForm(data=request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect("post_detail", post.pk)
+        if post.author == request.user:
+            form = forms.PostCreateForm(data=request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect("post_detail", post.pk)
 
-        return render(
-                    request=request,
-                    template_name="posts/post_update.html",
-                    context = {"form":form}
-                )
+            return render(
+                        request=request,
+                        template_name="posts/post_update.html",
+                        context = {"form":form}
+                    )
+
+        else:
+            return redirect("main_page")
 
 
 class PostDeleteView(View):
     def get(self, request, pk):
         post = Post.objects.get(pk=pk)
-        return render(
-            request=request,
-            template_name="posts/post_delete.html",
-            context={"post":post}
-        )
+        if post.author == request.user:
+            return render(
+                request=request,
+                template_name="posts/post_delete.html",
+                context={"post":post}
+            )
+
+        else:
+            return redirect("main_page")
 
     def post(self, request, pk):
         post = Post.objects.get(pk=pk)
-        post.delete()
-        return redirect("post_list")
+        if post.author == request.user:
+            post.delete()
+            return redirect("post_list")
+        else:
+            return redirect("main_page")
+
+
+class CommentUpdateView(View):
+    def get(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        if comment.author == request.user:
+            form = forms.CommentForm(data=None, instance=comment)
+            return render(
+                request=request,
+                template_name="posts/comment_update.html",
+                context={"form":form, "comment":comment}
+            )
+
+        else:
+            return redirect("main_page")
+
+    def post(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        if comment.author == request.user:
+            form = forms.CommentForm(data=request.POST, instance=comment)
+            if form.is_valid():
+                form.save()
+                return redirect("post_detail", comment.post.pk)
+
+            return render(
+                request=request,
+                template_name="posts/comment_update.html",
+                context={"form":form, "comment":comment}
+                )
+
+        else:
+            return redirect("main_page")
+
+
+class CommentDeleteView(View):
+    def get(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        if comment.author == request.user:
+            return render(
+                request=request,
+                template_name="posts/comment_delete.html",
+                context={"comment":comment}
+            )
+
+        else:
+            return redirect("main_page")
+
+    def post(self, request, pk):
+        comment = Comment.objects.get(pk=pk)
+        if comment.author == request.user:
+            post_pk = comment.post.pk
+            comment.delete()
+            return redirect("post_detail", post_pk)
+
+        else:
+            return redirect("main_page")
